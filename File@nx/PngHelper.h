4006553095fcfe43d0e56f29f55cc7a6f5bea1c9
abredo <juliano@julianodeiMac.local>
2016-03-31 15:28:14 +0800

    Get image size dynamically

---
+++ File@nx/PngHelper.h
@@ -0,0 +1,15 @@
+//
+//  PngHelper.h
+//  File@nx
+//
+//  Created by juliano on 3/31/16.
+//  Copyright © 2016 Chindle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PngHelper : NSObject
+
++(CGSize)getPNGSize:(NSString *)filePath;
+
+@end
---
+++ File@nx/PngHelper.m
@@ -0,0 +1,51 @@
+//
+//  PngHelper.m
+//  File@nx
+//
+//  Created by juliano on 3/31/16.
+//  Copyright © 2016 Chindle. All rights reserved.
+//
+
+#import "PngHelper.h"
+//#include <stdio.h>
+//#include <fcntl.h>
+//#include <unistd.h>
+//#include <arpa/inet.h>
+//#include <err.h>
+
+@implementation PngHelper
+
++(CGSize)getPNGSize:(NSString *)filePath
+{
+    if (!filePath.length) {
+        return CGSizeZero;
+    }
+    int fd;
+    uint32_t height = 0, width = 0;
+        if ((fd = open(filePath.UTF8String, O_RDONLY)) == -1)
+        {
+            printf("Error occurs when opening file");
+            return CGSizeZero;
+        }
+        if (lseek(fd, 16, SEEK_SET) == -1)
+        {
+            return CGSizeZero;
+        }
+        if (read(fd, &width, 4) < 1)
+        {
+            return CGSizeZero;
+        }
+        if (read(fd, &height, 4) < 1)
+        {
+            return CGSizeZero;
+        }
+        if (close(fd) == -1)
+        {
+            printf("Error occurs when closing file");
+//            return CGSizeZero;
+        }
+
+    return CGSizeMake(htonl(width), htonl(height));
+}
+
+@end
